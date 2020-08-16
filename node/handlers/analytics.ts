5724626b4a4f85cc11d2ab371a828e8b9a00f990
Tanivia Timbo <tanivia@gmail.com>
2020-08-16 00:57:56 -0300

    issue 6

--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,8 +1,10 @@
 export async function analytics(ctx: Context, next: () => Promise<any>) {
+  const {
+    clients: { analytics },
+  } = ctx
+  ctx.status = 200
   if (ctx.method.toUpperCase() === 'GET') {
-    ctx.status = 200
-    ctx.body = 'OK'
-
+    ctx.body = await analytics.getLiveUsers()
     ctx.set('cache-control', 'no-cache')
   }
   await next()
